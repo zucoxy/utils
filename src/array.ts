e7c0c96fa58b566f26c0a4d65363836412fb7fcc
zhuchunyu <zhuchunyu@chinawatermeter.com>
2023-09-01 12:54:58 +0800

    chore: arrayToOption 支持数组对象格式化

--- src/array.ts
+++ src/array.ts
@@ -3,25 +3,31 @@ import { isObject } from './is';
 
 /**
  * 传入一个数组，返回 labelValue 格式的数组数据
- * @param arr - 1. string[]  2. number[]  3. [value, label][] 4. {value: label}[]
+ * @param array - 1. string[]  2. number[]  3. [value, label][] 4. {value: label}[]
  */
-export const arrayToOption = <V>(arr: any[]): Required<LabelValue<V>>[] => {
-  return arr.map(item => {
-    let options: Required<LabelValue<V>> = { value: item, label: item };
-    if (Array.isArray(item)) {
-      options = { value: item[0], label: item[1] };
-    } else if (isObject(item)) {
-      if (Object.hasOwn(item, 'value') && Object.hasOwn(item, 'label')) {
-        options = item as LabelValue;
+const arrayToOption = <V>(array: any[]): Required<LabelValue<V>>[] => {
+  if (!Array.isArray(array)) {
+    console.warn('TypeError: array excepted to be Array');
+    return array;
+  }
+  return array
+    .map(item => {
+      let options: Required<LabelValue<V>> = { value: item, label: item };
+      if (Array.isArray(item)) {
+        options = { value: item[0], label: item[1] };
+      } else if (isObject(item)) {
+        if (Object.hasOwn(item, 'value') && Object.hasOwn(item, 'label')) {
+          options = item as LabelValue;
+        } else {
+          const entry: any[] = Object.entries(item);
+          return entry.map(i => ({ value: i[0], label: i[1] }));
+        }
       } else {
-        const entry: any[] = Object.entries(item);
-        options = { value: entry[0][0], label: entry[0][1] };
+        options = { value: item, label: item };
       }
-    } else {
-      options = { value: item, label: item };
-    }
-    return options;
-  });
+      return options;
+    })
+    .flat();
 };
 
 /**
