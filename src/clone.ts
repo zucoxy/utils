b381e8fba52e7e2c479edf1c09e258919496a20a
zhuchunyu <zhuchunyu@chinawatermeter.com>
2023-09-06 23:42:57 +0800

    faat: 新增深拷贝函数

---
+++ src/clone.ts
@@ -0,0 +1,120 @@
+import { toString } from './base';
+import {
+  isArray,
+  isBoolean,
+  isDate,
+  isError,
+  isMap,
+  isNumber,
+  isObject,
+  isRegExp,
+  isSet,
+  isString,
+  isSymbol
+} from './is';
+
+const mapTag = '[object Map]';
+const setTag = '[object Set]';
+const arrayTag = '[object Array]';
+const objectTag = '[object Object]';
+
+const deepTags = [mapTag, setTag, arrayTag, objectTag];
+
+const boolTag = '[object Boolean]';
+const dateTag = '[object Date]';
+const errorTag = '[object Error]';
+const numberTag = '[object Number]';
+const regexpTag = '[object RegExp]';
+const stringTag = '[object String]';
+const symbolTag = '[object Symbol]';
+
+function forEach(array: any, iteratee: (value: any, key: any) => void) {
+  let index = -1;
+  const length = array.length;
+  while (++index < length) {
+    iteratee(array[index], index);
+  }
+  return array;
+}
+
+function getInit(target: any) {
+  const Ctor = target.constructor;
+  return new Ctor();
+}
+function cloneSymbol(target: any) {
+  return Object(Symbol.prototype.valueOf.call(target));
+}
+function cloneReg(target: any) {
+  const reFlags = /\w*$/;
+  const result = new target.constructor(target.source, reFlags.exec(target));
+  result.lastIndex = target.lastIndex;
+  return result;
+}
+function cloneOtherType(target: any) {
+  const Ctor = target.constructor;
+  switch (target) {
+    case isBoolean(target):
+    case isNumber(target):
+    case isString(target):
+    case isError(target):
+    case isDate(target):
+      return new Ctor(target);
+    case isRegExp(target):
+      return cloneReg(target);
+    case isSymbol(target):
+      return cloneSymbol(target);
+    default:
+      return null;
+  }
+}
+
+export function deepClone(target: any, map = new WeakMap()) {
+  // 克隆原始类型
+  if (!isObject(target)) {
+    return target;
+  }
+
+  // 初始化
+  const type = toString(target);
+  let cloneTarget: any;
+  if (deepTags.includes(type)) {
+    cloneTarget = getInit(target);
+  } else {
+    return cloneOtherType(target);
+  }
+
+  // 防止循环引用
+  if (map.get(target)) {
+    return map.get(target);
+  }
+  map.set(target, cloneTarget);
+
+  // 克隆set
+  if (isSet(target)) {
+    cloneTarget = getInit(target);
+    target.forEach(value => {
+      cloneTarget.add(deepClone(value, map));
+    });
+    return cloneTarget;
+  }
+
+  // 克隆map
+  if (isMap(target)) {
+    cloneTarget = getInit(target);
+    target.forEach((value, key) => {
+      cloneTarget.set(key, deepClone(value, map));
+    });
+    return cloneTarget;
+  }
+
+  // 克隆对象和数组
+  const keys = isArray(target) ? undefined : Object.keys(target);
+  forEach(keys || target, (value, key) => {
+    if (keys) {
+      key = value;
+    }
+    cloneTarget[key] = deepClone((target as any)[key], map);
+  });
+
+  return cloneTarget;
+}
--- src/is.ts
+++ src/is.ts
@@ -8,9 +8,14 @@ export const isString = (val: unknown): val is string => typeof val === 'string'
 export const isObject = (val: any): val is object => toString(val) === '[object Object]';
 export const isUndefined = (val: any): val is undefined => toString(val) === '[object Undefined]';
 export const isNull = (val: any): val is null => toString(val) === '[object Null]';
+export const isSymbol = (val: any): val is Symbol => toString(val) === '[object Symbol]';
 export const isRegExp = (val: any): val is RegExp => toString(val) === '[object RegExp]';
 export const isDate = (val: any): val is Date => toString(val) === '[object Date]';
-export const isFile = (val: any): val is Date => toString(val) === '[object File]';
+export const isFile = (val: any): val is File => toString(val) === '[object File]';
+export const isError = (val: any): val is Error => toString(val) === '[object Error]';
+export const isMap = (val: any): val is Map<any, any> => toString(val) === '[object Map]';
+export const isSet = (val: any): val is Set<any> => toString(val) === '[object Set]';
+export const isArray = (val: any): val is Array<any> => toString(val) === '[object Array]';
 
 // @ts-ignore
 export const isWindow = (val: any): boolean => typeof window !== 'undefined' && toString(val) === '[object Window]';
